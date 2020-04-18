e3a191eae148433771f4e73b0b436873ea1ee411
Samin <khandakersamin23@gmail.com>
2020-04-18 14:12:34 -0400

    MovementStuff

---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    public GameObject Player;
+    public CharacterController controller;
+    public float speed = 10f;
+    public float gravity = 9.81f;
+    public Rigidbody Phycis;
+    public Vector3 velocity;
+    // Start is called before the first frame updates
+    void Start()
+    {
+        Phycis = Player.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space")){
+            velocity.y = 5f;
+        }
+
+
+    }
+}
